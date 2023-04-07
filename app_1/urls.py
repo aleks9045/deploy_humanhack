5df0866db43335bbb190a5b770d7ad355a2bf9fb
aleks <88434190+aleks9045@users.noreply.github.com>
2023-04-07 23:28:20 +0300

    Update urls.py

--- app_1/urls.py
+++ app_1/urls.py
@@ -3,6 +3,6 @@ from .views import *
 from app_1.views import *
 
 urlpatterns = [
-    path('API/math/theory/1?format=json', Math_theoryAPIView.as_view()),
-    path('API/math/task/1?format=json', Math_taskAPIView.as_view()),
+    path('API/math/theory/1', Math_theoryAPIView.as_view()),
+    path('API/math/task/1', Math_taskAPIView.as_view()),
 ]
