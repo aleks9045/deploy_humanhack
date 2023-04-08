1b03cb30c2fb7228fb8e36f0387f2df9ec2b33e9
aleks <88434190+aleks9045@users.noreply.github.com>
2023-04-08 20:23:11 +0300

    Update urls.py

--- app_1/urls.py
+++ app_1/urls.py
@@ -5,8 +5,8 @@ from app_1.views import *
 urlpatterns = [
     path('API/math/theory/1', Math_theoryAPIView.as_view()),
     path('API/math/task/1', Math_taskAPIView.as_view()),
-    path('API/animals/theory/1', English_theoryAPIView.as_view()),
-    path('API/animals/task/1', English_taskAPIView.as_view()),
+    path('API/animals/theory/1', Animals_theoryAPIView.as_view()),
+    path('API/animals/task/1', Animals_taskAPIView.as_view()),
     path('API/read/theory/1', Read_theoryAPIView.as_view()),
     path('API/read/task/1', Read_taskAPIView.as_view()),
     path('API/english/theory/1', English_theoryAPIView.as_view()),
