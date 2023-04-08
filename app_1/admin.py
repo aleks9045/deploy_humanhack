f297ab45b4c0eef2b93dac6ff00052f57bcbc178
aleks <88434190+aleks9045@users.noreply.github.com>
2023-04-08 07:17:21 +0300

    Update admin.py

--- app_1/admin.py
+++ app_1/admin.py
@@ -4,3 +4,9 @@ from .models import *
 
 admin.site.register(Math_theory)
 admin.site.register(Math_task)
+admin.site.register(Animals_theory)
+admin.site.register(Animals_task)
+admin.site.register(Read_theory)
+admin.site.register(Read_task)
+admin.site.register(English_theory)
+admin.site.register(English_task)
