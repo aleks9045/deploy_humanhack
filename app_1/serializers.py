9efd6f2249e9422291a1f6e62dbb3cc46c943a91
aleks <88434190+aleks9045@users.noreply.github.com>
2023-04-08 07:18:31 +0300

    Update serializers.py

--- app_1/serializers.py
+++ app_1/serializers.py
@@ -12,3 +12,45 @@ class Math_taskSerializer(serializers.Serializer):
     number_task = serializers.IntegerField()
     condition = serializers.CharField()
     answer = serializers.CharField(max_length=255)
+
+
+class Animals_theorySerializer(serializers.Serializer):
+    number_theory = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    condition = serializers.CharField()
+
+
+class Animals_taskSerializer(serializers.Serializer):
+    number_task = serializers.IntegerField()
+    text = serializers.CharField()
+    condition = serializers.CharField()
+    answer = serializers.CharField(max_length=255)
+
+
+class Read_theorySerializer(serializers.Serializer):
+    number_theory = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    condition = serializers.CharField()
+
+
+class Read_taskSerializer(serializers.Serializer):
+    number_task = serializers.IntegerField()
+    text = serializers.CharField()
+    condition = serializers.CharField()
+    answer = serializers.CharField(max_length=255)
+
+
+class English_theorySerializer(serializers.Serializer):
+    number_theory = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    condition = serializers.CharField()
+
+
+class English_taskSerializer(serializers.Serializer):
+    number_task = serializers.IntegerField()
+    text = serializers.CharField()
+    condition = serializers.CharField()
+    answer = serializers.CharField(max_length=255)
