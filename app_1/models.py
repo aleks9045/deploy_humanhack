a8a026c1729319eb8e6cadde56200e06ec3db00f
aleks <88434190+aleks9045@users.noreply.github.com>
2023-04-08 07:18:08 +0300

    Update models.py

--- app_1/models.py
+++ app_1/models.py
@@ -18,3 +18,64 @@ class Math_task(models.Model):
 
     def __str__(self):
         return self.condition
+
+
+
+class Animals_theory(models.Model):
+    number_theory = models.IntegerField()
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    condition = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+class Animals_task(models.Model):
+    number_task = models.IntegerField()
+    text = models.TextField()
+    condition = models.TextField()
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.condition
+
+
+class Read_theory(models.Model):
+    number_theory = models.IntegerField()
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    condition = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+class Read_task(models.Model):
+    number_task = models.IntegerField()
+    text = models.TextField()
+    condition = models.TextField()
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.condition
+
+
+class English_theory(models.Model):
+    number_theory = models.IntegerField()
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    condition = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+class English_task(models.Model):
+    number_task = models.IntegerField()
+    text = models.TextField()
+    condition = models.TextField()
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.condition
