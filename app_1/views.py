1892219024c91e8fb7689841f15a2bc0e30b1ba7
aleks <88434190+aleks9045@users.noreply.github.com>
2023-04-08 07:19:18 +0300

    Update views.py

--- app_1/views.py
+++ app_1/views.py
@@ -10,7 +10,45 @@ class Math_theoryAPIView(APIView):
         w = Math_theory.objects.all()
         return Response({'theory': Math_theorySerializer(w, many=True).data})
 
+
 class Math_taskAPIView(APIView):
     def get(self, request):
         w = Math_task.objects.all()
         return Response({'task': Math_taskSerializer(w, many=True).data})
+
+
+class Animals_theoryAPIView(APIView):
+    def get(self, request):
+        w = Animals_theory.objects.all()
+        return Response({'theory': Animals_theorySerializer(w, many=True).data})
+
+
+class Animals_taskAPIView(APIView):
+    def get(self, request):
+        w = Animals_theory.objects.all()
+        return Response({'task': Animals_taskSerializer(w, many=True).data})
+
+
+class Read_theoryAPIView(APIView):
+    def get(self, request):
+        w = Read_theory.objects.all()
+        return Response({'theory': Read_theorySerializer(w, many=True).data})
+
+
+class Read_taskAPIView(APIView):
+    def get(self, request):
+        w = Read_task.objects.all()
+        return Response({'task': Read_taskSerializer(w, many=True).data})
+
+
+class English_theoryAPIView(APIView):
+    def get(self, request):
+        w = English_theory.objects.all()
+        return Response({'theory': English_theorySerializer(w, many=True).data})
+
+
+class English_taskAPIView(APIView):
+    def get(self, request):
+        w = English_task.objects.all()
+        return Response({'task': English_taskSerializer(w, many=True).data})
+
